fdbf4be63827ac83ab8aaadff7e395808cae37ca
abinbabu <abinsee@gmail.com>
2016-09-27 09:08:13 +0530

    added all controllers

--- CollaborationBackend/src/main/java/com/niit/config/AppInitializer.java
+++ CollaborationBackend/src/main/java/com/niit/config/AppInitializer.java
@@ -6,7 +6,7 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
 	@Override
 	protected Class[] getRootConfigClasses() {
-		System.out.println("Test");
+		System.out.println("Testing");
 		return new Class[] { ApplicationContextConfig.class};
 	}
 
@@ -18,7 +18,7 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
 	@Override
 	protected String[] getServletMappings() {
-		System.out.println("Working");
+		System.out.println("Working....");
 		return new String[] { "/" };
 	}
 
